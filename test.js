30e635fc9fa9414069367d2c89c8c19e96520aef
Wupei <2468394520@qq.com>
2019-04-19 13:52:08 +0800

    js练习文件

---
+++ test.js
@@ -0,0 +1,12 @@
+var str = "中国"; 
+alert( "这是test.js中的变量：" + str );
+
+
+window.onload = function(){
+	alert('我已经被加载啦！');
+	alert("获取box的值是：", document.getElementById("box").innerText);
+	//document.getElementById("box").innerText ="122222222222222222";
+}
+
+
+//alert("获取box的值是：", document.getElementById("box"));
