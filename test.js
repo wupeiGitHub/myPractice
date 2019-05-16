129672b10d91c36699f0b8cd888cef60678a6388
Wupei <2468394520@qq.com>
2019-05-16 16:18:25 +0800

    ÐÂÔöjsÁ·ϰÎļþ

--- test.js
+++ test.js
@@ -2,11 +2,14 @@ var str = "中国";
 alert( "这是test.js中的变量：" + str );
 
 
-window.onload = function(){
-	alert('我已经被加载啦！');
-	alert("获取box的值是：", document.getElementById("box").innerText);
-	//document.getElementById("box").innerText ="122222222222222222";
-}
+// window.onload = function(){
+// 	alert('我已经被加载啦！');
+// 	alert("获取box的值是：", document.getElementById("box").innerText);
+// 	//document.getElementById("box").innerText ="122222222222222222";
+// }
 
+function test(){
+	alert('test函数已经被调用了');
+}
 
 //alert("获取box的值是：", document.getElementById("box"));
